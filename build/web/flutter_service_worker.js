578f6c981e68d4a49bf9c3bb63b84ade3728e445
sanadidari <schatwiti@gmail.com>
2025-12-17 18:34:23 +0100

    🐛 Correction du nom d’image Footer (bug 404)

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -35,7 +35,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "1b8c19556021c5355a95861a8706db86
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "a816072223931860487c0fad730a13f5",
+"flutter_bootstrap.js": "aa4d3e15a1a2af538285d0f6eda6d60b",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
