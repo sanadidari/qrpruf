f50e930accb4f1ccc98bdccb2408e4bb1683e189
sanadidari <schatwiti@gmail.com>
2025-12-17 12:34:29 +0100

    force rebuild footer fix

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -47,7 +47,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "97d08246faec1b8175eba1ad5de8c2a9
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "43b18d894273cc1ea238529e53eb41a2",
+"flutter_bootstrap.js": "658a19890a9018029dc9ea072167ba5b",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
