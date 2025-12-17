cce04baa755c454e9771753e8f8f198f41369590
sanadidari <schatwiti@gmail.com>
2025-12-17 13:10:19 +0100

    Build web with correct footer

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "688146042"
+    serviceWorkerVersion: "823072117"
   }
 });
--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -47,7 +47,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "4b25fd3e4f525e223bf46619a14c1e35
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "d9263b2382b173c27b0e3b3896f266e1",
+"flutter_bootstrap.js": "6db8fa40f331e4004c709e9a835fc383",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
