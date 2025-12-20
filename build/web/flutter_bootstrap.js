640dbd9f25e928d6808d79303d45327430c87c88
sanadidari <schatwiti@gmail.com>
2025-12-20 14:02:43 +0100

    build web

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1527ae0ec577a4ef50e65f6fefcfc1326707d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3765173308"
+    serviceWorkerVersion: "4022775968"
   }
 });
